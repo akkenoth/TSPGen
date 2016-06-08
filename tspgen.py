ba134efdd0a92b03744d444b0aa4247c4e069e54
mjbogusz <mjbogusz+github@gmail.com>
2016-06-08 19:41:57 +0200

    tspgen: added displacement mutator sample

--- tspgen.py
+++ tspgen.py
@@ -4,6 +4,7 @@ import sys
 
 from Map import Map
 from Operators.Crossing import EdgeCrosser
+from Operators.Mutation import DisplacementMutator
 from Population import Population
 from Population import Unit
 from Selection import TournamentSelector
@@ -14,13 +15,18 @@ def main():
 	for city in problemMap.cities:
 		print("City " + str(city) + ", connections to: " + str(city.connections))
 
-	crosser = EdgeCrosser()
 	unit1 = Unit.generateUnit(problemMap)
 	print(unit1.path)
 	unit2 = Unit.generateUnit(problemMap)
 	print(unit2.path)
+
+	crosser = EdgeCrosser()
 	unit3 = crosser.make(problemMap, [unit1, unit2])
 	print(unit3.path)
+
+	mutator = DisplacementMutator()
+	unit4 = mutator.make(problemMap, unit3)
+	print(unit4.path)
 
 	sys.exit(0)
 
