3aaea443684d844c474abd15a99088c46cf04f31
mjbogusz <mjbogusz+github@gmail.com>
2016-06-09 13:32:06 +0200

    tspgen: added mutating test unit with InversionMutator

--- tspgen.py
+++ tspgen.py
@@ -4,7 +4,7 @@ import sys
 
 from Map import Map
 from Operators.Crossing import EdgeCrosser
-from Operators.Mutation import DisplacementMutator
+from Operators.Mutation import DisplacementMutator, InversionMutator
 from Population import Population
 from Population import Unit
 from Selection import TournamentSelector
@@ -15,18 +15,26 @@ def main():
 	for city in problemMap.cities:
 		print("City " + str(city) + ", connections to: " + str(city.connections))
 
+	print("Generated units:")
 	unit1 = Unit.generateUnit(problemMap)
 	print(unit1.path)
 	unit2 = Unit.generateUnit(problemMap)
 	print(unit2.path)
 
+	print("Child of generated units:")
 	crosser = EdgeCrosser()
 	unit3 = crosser.make(problemMap, [unit1, unit2])
 	print(unit3.path)
 
-	mutator = DisplacementMutator()
-	unit4 = mutator.make(problemMap, unit3)
+	print("Displacement mutated child:")
+	displacementMutator = DisplacementMutator()
+	unit4 = displacementMutator.make(problemMap, unit3)
 	print(unit4.path)
+
+	print("Inversion mutated child:")
+	inversionMutator = InversionMutator()
+	unit5 = inversionMutator.make(problemMap, unit3)
+	print(unit5.path)
 
 	sys.exit(0)
 
