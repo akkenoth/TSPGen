020db9b9f3d07312b93cff3d79ca56eb219f7db0
gizmoy <m.giemza55@gmail.com>
2016-06-10 03:04:02 +0200

    Update tspgen.py

--- tspgen.py
+++ tspgen.py
@@ -19,7 +19,7 @@ def main():
 	# for city in problemMap2.cities:
 	# 	print("City " + str(city) + ", connections to: " + str(city.connections))
 
-
+	"""
 	print("Map1:")
 	print("Generated units:")
 	unit1 = Unit.generateUnitRand(problemMap)
@@ -63,6 +63,30 @@ def main():
 	inversionMutator = InversionMutator()
 	unit10 = inversionMutator.make(problemMap, unit8)
 	print(unit10.path)
+	"""
+	
+	for city in problemMap.cities:
+		print("City " + str(city) + ", connections to: " + str(city.connections))
+
+	units = []
+	populationSize = 10
+	while len(units) < populationSize:
+		units.append(Unit.generateUnit(problemMap))
+
+	population = Population(populationSize, units)
+
+	print("Initial population: ")
+	for unit in population.units:
+		print(unit.path, " | ", unit.fitness)
+	print()
+
+	selector = TournamentSelector()
+	crosser = EdgeCrosser()
+	mutator = DisplacementMutator()
+	evolution = Evolution(problemMap, population, selector, crosser, mutator)
+
+	evolution.make(10)
+
 
 	sys.exit(0)
 
