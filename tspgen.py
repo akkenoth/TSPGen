40f83d40567e5cb4aa42d90ee21120c03c59b3eb
mjbogusz <mjbogusz+github@gmail.com>
2016-06-11 01:11:03 +0200

    tspgen: removed old and commented test mutations etc, added unit generation benchmarks (commented out)

--- tspgen.py
+++ tspgen.py
@@ -10,79 +10,51 @@ from Selection import TournamentSelector
 
 def main():
 	problemMap = Map.generateCNN(50, 15)
-	print("Generated %d cities:" % problemMap.size)
-	for city in problemMap.cities:
-		print("City " + str(city) + ", connections to: " + str(city.connections))
-	
+	# print("Generated %d cities:" % problemMap.size)
+	# for city in problemMap.cities:
+	# 	print("City " + str(city) + ", connections to: " + str(city.connections))
+
+	# Unit generation 'benchmark'
+	# print("Generated units:")
+	# unit1 = Unit.generateUnitRand(problemMap)
+	# print("Random: cost {0}, fitness {1}".format(unit1.cost, unit1.fitness))
+	# unit2 = Unit.generateUnitDSRandom(problemMap)
+	# print("DS Random: cost {0}, fitness {1}".format(unit2.cost, unit2.fitness))
+	# unit3 = Unit.generateUnitDSGreedy(problemMap)
+	# print("DS Greedy: cost {0}, fitness {1}".format(unit3.cost, unit3.fitness))
+
+	# units = []
+	# cost = 0
+	# fitness = 0.0
+	# for i in range(10):
+	# 	unit = Unit.generateUnitRand(problemMap)
+	# 	print("Unit {0}: fitness {1}, cost {2}".format(i+1, unit.fitness, unit.cost))
+	# 	fitness += unit.fitness
+	# 	cost += unit.cost
+	# 	units.append(unit)
+	# print("Avg fitness: {0}".format(fitness/10))
+	# print("Avg cost: {0}".format(cost/10))
+
 	"""
 	problemMap2 = Map.readFromFile("samples/sampleMap.json")
 	print("Read %d cities from file:" % problemMap2.size)
-	for city in problemMap2.cities:
-		print("City " + str(city) + ", connections to: " + str(city.connections))
-	"""
-
-	"""
-	print("Map1:")
-	print("Generated units:")
-	unit1 = Unit.generateUnitRand(problemMap)
-	print(unit1.path)
-	unit2 = Unit.generateUnitRand(problemMap)
-	print(unit2.path)
-
-	print("Child of generated units:")
-	crosser = EdgeCrosser()
-	unit3 = crosser.make(problemMap, [unit1, unit2])
-	print(unit3.path)
-
-	print("Displacement mutated child:")
-	displacementMutator = DisplacementMutator()
-	unit4 = displacementMutator.make(problemMap, unit3)
-	print(unit4.path)
-
-	print("Inversion mutated child:")
-	inversionMutator = InversionMutator()
-	unit5 = inversionMutator.make(problemMap, unit3)
-	print(unit5.path)
-
-	print("Map2:")
-	print("Generated units:")
-	unit6 = Unit.generateUnitRand(problemMap2)
-	print(unit6.path)
-	unit7 = Unit.generateUnitRand(problemMap2)
-	print(unit7.path)
-
-	print("Child of generated units:")
-	crosser = EdgeCrosser()
-	unit8 = crosser.make(problemMap, [unit6, unit7])
-	print(unit8.path)
-
-	print("Displacement mutated child:")
-	displacementMutator = DisplacementMutator()
-	unit9 = displacementMutator.make(problemMap, unit8)
-	print(unit9.path)
-
-	print("Inversion mutated child:")
-	inversionMutator = InversionMutator()
-	unit10 = inversionMutator.make(problemMap, unit8)
-	print(unit10.path)
 	"""
 	
 	units = []
-	populationSize = 10
-	while len(units) < populationSize:
+	for i in range(10):
 		units.append(Unit.generateUnitRand(problemMap))
-	population = Population(populationSize, units)
+	population = Population(10, units)
 
-	print("Initial population: ")
-	for i in range(len(population.units)):
-		unit = population.units[i]
-		print("{0}: {1} | {2}".format(i, unit.path, unit.fitness))
+	# print("Initial population: ")
+	# for i in range(len(population.units)):
+	# 	unit = population.units[i]
+	# 	print("{0}: {1} | {2}".format(i, unit.path, unit.fitness))
 
-	selector = TournamentSelector(6, 2)
-	crosser = EdgeCrosser()
-	mutator = DisplacementMutator(0.1)
-	evolution = Evolution(problemMap, population, selector, crosser, mutator)
-	evolution.make(10)
+	# selector = TournamentSelector(6, 2)
+	# crosser = EdgeCrosser()
+	# mutator = DisplacementMutator(0.1)
+	# evolution = Evolution(problemMap, population, selector, crosser, mutator)
+	# evolution.make(10)
 
 	sys.exit(0)
 
