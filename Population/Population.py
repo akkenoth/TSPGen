96704104f2fba8c12f20ddd70ed333818c7fd7cf
mjbogusz <mjbogusz+github@gmail.com>
2016-06-10 13:55:32 +0200

    Population/Population: added some default parameters, improved adding unit, added addUnitAndExpand (adds unit and increases population size)

--- Population/Population.py
+++ Population/Population.py
@@ -6,31 +6,36 @@ class Population(object):
     elitism = False
     mutationRate = 0.1
 
-    def __init__(self, size, units = None):
+    def __init__(self, size = 0, units = None):
         self.size = size
         if units is None:
             self.units = []
             self.sorted = False
         else:
             self.units = units
-            self.units.sort(key=lambda u: u.fitness, reverse=True)
-            self.sorted = True;
+            self.units.sort(key=lambda unit: unit.fitness, reverse=True)
+            self.sorted = True
 
     def addUnit(self, unit):
-        if len(self.units) < self.size:
-            if unit not in self.units:
-                self.units.append(unit)
-                self.sorted = False
-            else:
-                pass
-        else:
-            pass
+        if len(self.units) >= self.size:
+            return    
+        if unit in self.units:
+            return
+        self.units.append(unit)
+        self.sorted = False
 
-    def bestUnit(self, position):
+    def addUnitAndExpand(self, unit):
+        if unit in self.units:
+            return
+        self.units.append(unit)
+        self.size += 1
+        self.sorted = False
+
+    def bestUnit(self, position = 0):
         if position >= self.size:
             position = self.size - 1
         if self.sorted == False:
-            self.units.sort(key = Unit.calcFitness)
+            self.units.sort(key = lambda unit: unit.fitness, reverse=True)
             self.sorted = True
 
         return self.units[position]
